752109d1e89638fd530233f10db30a6034d00842
F-park <52167622+F-park@users.noreply.github.com>
2024-06-01 18:47:57 +0800

    add type hint for argparse

--- whats_new/__init__.py
+++ whats_new/__init__.py
@@ -1,9 +1,15 @@
-from argparse import ArgumentParser
+from argparse import ArgumentParser, Namespace
+
+
+class Args(Namespace):
+    update: bool
+    download: bool
 
 
 def main():
     parser = ArgumentParser()
     parser.add_argument("-u", "--update", action="store_true")
-    args = parser.parse_args()
+    parser.add_argument("-d", "--download", action="store_true")
+    args = parser.parse_args(namespace=Args())
     if args.update:
         print("hello world")
