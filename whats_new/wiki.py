40cd431dce7721c1165c6a2d16750f362bfa8722
F-park <52167622+F-park@users.noreply.github.com>
2024-09-12 23:43:36 +0800

    switch mwclient

--- whats_new/wiki.py
+++ whats_new/wiki.py
@@ -1,12 +1,11 @@
-import os
 import re
 from datetime import date
+from os import environ
 from typing import TYPE_CHECKING
 
-from pwiki import wiki
+from mwclient import Site
 
 from .parser import WhatsNewParser
-from .types import Vers
 
 if TYPE_CHECKING:
     from .chillyroom import Apk
@@ -14,13 +13,13 @@ if TYPE_CHECKING:
 
 class Wiki:
     def __init__(self):
-        self.vers: Vers
-        password = os.environ.get("HUIJIWIKI_PASSWORD")
-        self.wiki = wiki.Wiki("yqqs.huijiwiki.com", "慕棱", password or "")
+        KEY = environ.get("HUIJIWIKI_KEY")
+        PASSWORD = environ.get("HUIJIWIKI_PASSWORD")
+        self.site = Site("yqqs.huijiwiki.com", custom_headers={"X-authkey": KEY})
+        self.site.login("慕棱", PASSWORD)
 
     def find_vers(self, text: str) -> re.Match:
         if m := re.search(r"==(\d).(\d).(\d)", text):
-            self.vers = Vers(*m.groups())
             return m
 
         raise RuntimeError("维基找不到旧版本号")
@@ -31,11 +30,12 @@ class Wiki:
 
         page_name = "更新日志" if __debug__ else "Project:Sandbox/ML"
         print(f"更新页面为 {page_name}")
-        wiki_text = self.wiki.page_text(page_name)
+        page = self.site.pages[page_name]
+        page_text = page.text()
 
-        m = self.find_vers(wiki_text)
+        m = self.find_vers(page_text)
 
-        if tuple(map(int, apk.vers)) <= tuple(map(int, self.vers)):
+        if tuple(map(int, apk.vers)) <= tuple(map(int, m.groups())):
             print("已是最新版本")
             return
 
@@ -51,7 +51,7 @@ class Wiki:
             )
         )
 
-        if output_path := os.environ.get("GITHUB_OUTPUT"):
+        if output_path := environ.get("GITHUB_OUTPUT"):
             with open(output_path, "a") as out:
                 out.write("need_upload=1\n" f"vers={vers_str}\n")
             with open("whats_new.txt", "w") as f:
@@ -60,7 +60,6 @@ class Wiki:
         print(f"将更新至{vers_str}版本")
 
         insert_index = m.span()[0]
-        self.wiki.edit(
-            page_name,
-            wiki_text[:insert_index] + content + wiki_text[insert_index:],
+        page.edit(
+            page_text[:insert_index] + content + page_text[insert_index:], bot=False
         )
