07dbe631914345363af32a8fca4fb2b8de4000a8
F-park <52167622+F-park@users.noreply.github.com>
2024-09-19 21:18:30 +0800

    use github.ref_name

--- whats_new/github.py
+++ whats_new/github.py
@@ -1,4 +1,3 @@
-import subprocess
 from os import environ
 from typing import TYPE_CHECKING
 
@@ -14,17 +13,15 @@ def set_github_output(d: dict):
 
 
 def check_release_needed(apk: "Apk"):
-    old_vers_str = (
-        subprocess.run("git tag", shell=True, capture_output=True)
-        .stdout.decode()
-        .removesuffix("\n")
-    )
-    print(f"旧版本为：{old_vers_str}")
+    old_ver = environ.get("OLD_VER")
+    if not old_ver:
+        raise RuntimeError("无法获得旧版本号")
+    print(f"旧版本为：{old_ver}")
 
-    if tuple(map(int, apk.vers)) > tuple(map(int, old_vers_str.split("."))):
-        new_vers_str = ".".join(apk.vers)
-        print(f"将创建发行版：{new_vers_str}")
-        set_github_output({"need_upload": 1, "new_vers": new_vers_str})
+    if tuple(map(int, apk.vers)) > tuple(map(int, old_ver.split("."))):
+        new_ver = ".".join(apk.vers)
+        print(f"将创建发行版：{new_ver}")
+        set_github_output({"need_upload": 1, "new_ver": new_ver})
         return True
     else:
         print("不需要创建发行版")
